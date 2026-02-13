4070ed53acc9d79609ab46fc614daedee710c7ea
Saurav Sirswal <sauravsirswal11@gmail.com>
2026-02-13 10:56:48 +0530

    added idempotency logic for makePayment request

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,6 +2,8 @@ const { StatusCodes } = require("http-status-codes");
 const {BookingService} = require("../services")
 const {SuccessResponse, ErrorResponse} = require("../utils/common")
 
+const inMemoryDB = {}
+
 async function createBooking(req, res) {
     try {
         const response = await BookingService.createBooking({
@@ -23,11 +25,23 @@ async function createBooking(req, res) {
 
 async function makePayment(req, res) {
     try {
+        const idempotencyKey = req.headers["x-idempotency-key"];
+        if(!idempotencyKey){
+            return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json({message : "Idempotency key is required"});
+        }
+        if(inMemoryDB[idempotencyKey]){
+            return res
+                .status(StatusCodes.CONFLICT)
+                .json({message : "Cannot retry on a successful payment"});
+        }
         const response = await BookingService.makePayment({
             bookingId: req.body.bookingId,
             userId: req.body.userId,
             totalCost: req.body.totalCost
         });
+        inMemoryDB[idempotencyKey] = idempotencyKey
         SuccessResponse.data = response;
         return res
                 .status(StatusCodes.OK)
